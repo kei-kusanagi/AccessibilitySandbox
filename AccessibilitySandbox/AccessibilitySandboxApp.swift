220c10c540dccb629421ad5f2e0680d8373e2b7b
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-12 11:41:25 -0600

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Juan Carlos Robledo Morales on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandboxTests/AccessibilitySandboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxTests.swift
+//  AccessibilitySandboxTests
+//
+//  Created by Juan Carlos Robledo Morales on 12/11/24.
+//
+
+import Testing
+@testable import AccessibilitySandbox
+
+struct AccessibilitySandboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
